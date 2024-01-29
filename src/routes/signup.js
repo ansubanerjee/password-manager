d80715ea020f1ecde1d05a41eac3f2420cc336fd
ansubanerjee <103263719+ansubanerjee@users.noreply.github.com>
2024-01-29 23:54:13 +0530

    Update signup.js

--- src/routes/signup.js
+++ src/routes/signup.js
@@ -9,13 +9,13 @@ signup.get("/", (req, res)=>{
 
 signup.post("/",async (req,res)=>{
     const data = {
-        name: req.body.name,
-        email:req.body.email,
-        password: req.body.password
-    }
+        name: req.body.name.trim(),
+        email:req.body.email.trim(),
+        password: req.body.password.trim()
+    };
         try{
-            const check  = await Usercollection.findOne({name:req.body.name})
-            if (check.name == req.body.name){
+            const check  = await Usercollection.findOne({name:data.name})
+            if (check == req.body.name){
                 res.send("Already Existing Username")
             }
         }catch{
